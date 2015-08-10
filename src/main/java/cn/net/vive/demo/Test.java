d3cbbec815aa875ef96e72500239da6de2e5e1a9
jinghewang <jinghewang@163.com>
2015-08-10 12:24:30 +0800

    test

--- src/main/java/cn/net/vive/demo/Test.java
+++ src/main/java/cn/net/vive/demo/Test.java
@@ -1,7 +1,13 @@
 package cn.net.vive.demo;
 
+import org.aspectj.runtime.internal.cflowstack.ThreadStackImpl11;
+
 /**
  * Created by robin on 2015/8/10.
  */
 public class Test {
+    public  String getMsg(String msg){
+        String className = this.getClass().toString();
+        return className + "-hello world:" + msg;
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,14 @@
+<%@page import="cn.net.vive.demo.Test" %>
+<%
+    Test test = new Test();
+    String msg = test.getMsg("wjh");
+    pageContext.setAttribute("msg",msg);
+%>
+
+
 <html>
 <body>
 <h2>Hello World!2</h2>
+<h2>${msg}</h2>
 </body>
 </html>
